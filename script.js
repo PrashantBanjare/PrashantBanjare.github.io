83c093bc0db4249247fabcabe13f8aedcfc78c79
PrashantBanjare <prashantbanjare9822@gmail.com>
2026-04-03 18:37:26 +0530

    Portfolio website

---
+++ script.js
@@ -0,0 +1,48 @@
+// Welcome message in console
+console.log("Welcome to Prashant Banjare Portfolio");
+
+// Highlight active section in navbar
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop - 100) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+
+});
+
+
+// SCROLL ANIMATION
+const revealElements = document.querySelectorAll("section");
+
+window.addEventListener("scroll", () => {
+
+    revealElements.forEach(el => {
+
+        const windowHeight = window.innerHeight;
+        const elementTop = el.getBoundingClientRect().top;
+
+        if (elementTop < windowHeight - 100) {
+            el.classList.add("show");
+        }
+
+    });
+
+});
